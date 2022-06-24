84ee0dbae76f9d7b934000f2d642b6a59dcb45ae
azik <azik.gudratov@bp.com>
2022-06-24 17:09:00 +0100

    Allow setting route prefixes

--- ApplicationBuilderExtensions.cs
+++ ApplicationBuilderExtensions.cs
@@ -3,45 +3,62 @@ using System.Diagnostics;
 using System.Runtime;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 
 namespace Berberis.StatsReporters;
 
 public static class ApplicationBuilderExtensions
 {
-    public static IApplicationBuilder UseStats(this IApplicationBuilder appBuilder, IStatsReporterFactory statsReporterFactory)
+    public static void MapStats(this IEndpointRouteBuilder endpoints, IStatsReporterFactory statsReporterFactory, string prefix = "")
+    {
+        var handler = new StatsRequestHandler(statsReporterFactory);
+        MapStats(endpoints, handler, prefix);
+    }
+
+    public static IApplicationBuilder UseStats(this IApplicationBuilder appBuilder, IStatsReporterFactory statsReporterFactory, string prefix = "")
     {
         var handler = new StatsRequestHandler(statsReporterFactory);
 
         appBuilder.UseEndpoints(endpoints =>
         {
-            endpoints.MapGet("stats/systeminfo", handler.GetSystemInfo);
-            endpoints.MapGet("stats/networkinfo", handler.GetNetworkInfo);
-            endpoints.MapGet("stats/systemstats", handler.GetSystemStats);
-            endpoints.MapGet("stats/networkstats", handler.GetNetworkStats);
-            endpoints.MapGet("stats/reporters", handler.ListReporters);
-            endpoints.MapGet("stats/reporters/{source}", handler.GetReporterStats);
+            MapStats(endpoints, handler, prefix);
         });
 
         return appBuilder;
     }
 
-    public static IApplicationBuilder UseGCOperations(this IApplicationBuilder appBuilder)
+    private static void MapStats(IEndpointRouteBuilder endpoints, StatsRequestHandler handler, string prefix)
+    {
+        endpoints.MapGet($"{prefix}stats/systeminfo", handler.GetSystemInfo);
+        endpoints.MapGet($"{prefix}stats/networkinfo", handler.GetNetworkInfo);
+        endpoints.MapGet($"{prefix}stats/systemstats", handler.GetSystemStats);
+        endpoints.MapGet($"{prefix}stats/networkstats", handler.GetNetworkStats);
+        endpoints.MapGet($"{prefix}stats/reporters", handler.ListReporters);
+        endpoints.MapGet($"{prefix}stats/reporters/" + "{source}", handler.GetReporterStats);
+    }
+
+    public static IApplicationBuilder UseGCOperations(this IApplicationBuilder appBuilder, string prefix = "")
     {
         appBuilder.UseEndpoints(endpoints =>
         {
-            endpoints.MapGet("dbg/gccollect", async context =>
-            {
-                var sw = Stopwatch.StartNew();
-                GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
-                GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true, true);
-                GC.WaitForPendingFinalizers();
-                GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true, true);
-
-                sw.Stop();
-                await context.Response.WriteAsync($"GC took {sw.Elapsed.TotalMilliseconds:N1}ms");
-            });
+            MapGCOperations(endpoints, prefix);
         });
 
         return appBuilder;
+    }
+
+    public static void MapGCOperations(IEndpointRouteBuilder endpoints, string prefix)
+    {
+        endpoints.MapGet($"{prefix}dbg/gccollect", async context =>
+        {
+            var sw = Stopwatch.StartNew();
+            GCSettings.LargeObjectHeapCompactionMode = GCLargeObjectHeapCompactionMode.CompactOnce;
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true, true);
+            GC.WaitForPendingFinalizers();
+            GC.Collect(GC.MaxGeneration, GCCollectionMode.Forced, true, true);
+
+            sw.Stop();
+            await context.Response.WriteAsync($"GC took {sw.Elapsed.TotalMilliseconds:N1}ms");
+        });
     }
 }
